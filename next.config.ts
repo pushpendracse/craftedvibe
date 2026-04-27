81741d4367c904b1f2087c2389d1299565e92b34
PUSHPENDRAKUMAR <pushpendracse2024@gmail.com>
2026-04-27 23:38:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",   // ✅ MOST IMPORTANT
+
   images: {
+    unoptimized: true,   // ✅ required for static export
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    formats: ['image/webp'],
+    formats: ["image/webp"],
     qualities: [75, 80, 85, 90],
   },
 };
